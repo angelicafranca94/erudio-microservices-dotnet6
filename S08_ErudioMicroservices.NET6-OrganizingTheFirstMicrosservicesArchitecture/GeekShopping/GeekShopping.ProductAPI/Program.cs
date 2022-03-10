85efe5b2f3956b45dc8203b2abaddf7099b8e8aa
Angélica <angel@DESKTOP-LJEPODJ>
2022-03-10 15:18:44 -0300

    Adicionando dados no banco e imagens no reposiotorio.

--- S08_ErudioMicroservices.NET6-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ S08_ErudioMicroservices.NET6-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,10 +18,15 @@ UseMySql(connection,
     new MySqlServerVersion(
         new Version(8, 0, 28))));
 
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
 //As injeções devem estar antes dessa linha
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
