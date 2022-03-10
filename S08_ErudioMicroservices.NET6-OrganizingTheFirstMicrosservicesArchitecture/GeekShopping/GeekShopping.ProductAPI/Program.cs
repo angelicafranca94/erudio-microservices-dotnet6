6cf0c0b8e82438d66b94e1645aa7033d263d2c95
Angélica <angel@DESKTOP-LJEPODJ>
2022-03-10 18:33:06 -0300

    Feito a configuração para roteamento dos endpoints no projeto api que não estava configuração para atender a controllers

--- S08_ErudioMicroservices.NET6-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ S08_ErudioMicroservices.NET6-OrganizingTheFirstMicrosservicesArchitecture/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -10,20 +10,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
+
+#region injeção de dependencia
 
 var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
 
-builder.Services.AddDbContext<MySQLContext>(options => options.
+builder.Services.
+AddDbContext<MySQLContext>(options => options.
 UseMySql(connection,
     new MySqlServerVersion(
         new Version(8, 0, 28))));
 
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+#endregion
 
 //As injeções devem estar antes dessa linha
 var app = builder.Build();
@@ -35,28 +40,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
